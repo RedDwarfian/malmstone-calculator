1b9400e31d7d2366919c6348f9b4a8656d1de9ba
RedDwarfian <reddwarf86esb@gmail.com>
2026-08-18 18:14:06 -0400

    Making Zoneless. Insert Zenless Zone Zero joke here.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,4 @@
-import {
-  ApplicationConfig,
-  importProvidersFrom,
-  provideZoneChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics';
 
@@ -10,7 +6,6 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     importProvidersFrom(NgxGoogleAnalyticsModule.forRoot('G-FR7W221WM7')),
   ],
